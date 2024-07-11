863997895075838035a807998016c98394ac854d
Renan Silva <renansupermanox@gmail.com>
2024-07-11 19:01:35 +0100

    circular constrained brownian tree

---
+++ src/browniantree/BrownianTree.java
@@ -0,0 +1,58 @@
+package browniantree;
+
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrownianTree extends PApplet {
+    public static final float WIDTH = 800;
+    public static final float HEIGHT = 800;
+    private static final int PARTICLES = 8000;
+    private static final float DIAMETER = 4;
+    private final List<Particle> frozenParticles = new ArrayList<>();
+    private final List<Particle> movingParticles = new ArrayList<>();
+
+    public static void main(String[] args) {
+        PApplet.main("browniantree.BrownianTree");
+    }
+
+    public void settings() {
+        size((int)WIDTH, (int)HEIGHT);
+    }
+
+    public void setup() {
+        background(0);
+        noStroke();
+
+//        Particle initialParticle = new Particle(this, WIDTH/2, HEIGHT/2, DIAMETER);
+//        frozenParticles.add(initialParticle);
+//        initialParticle.draw();
+
+        for (float i = 0; i < TWO_PI; i += 0.01f) {
+            Particle p = new Particle(this, (cos(i) * 300) + WIDTH/2, (sin(i) * 300) + HEIGHT/2, DIAMETER);
+            frozenParticles.add(p);
+            p.draw();
+        }
+
+        for (int i = 0; i < PARTICLES; i++) {
+            movingParticles.add(createRandomParticle());
+        }
+    }
+
+    public void draw() {
+        for (int i = movingParticles.size() - 1; i >= 0 ; i--) {
+            Particle current = movingParticles.get(i);
+            current.move();
+            if(current.checkIfCollides(frozenParticles)) {
+                movingParticles.remove(current);
+                frozenParticles.add(current);
+                current.draw();
+            }
+        }
+    }
+
+    private Particle createRandomParticle() {
+        return new Particle(this, random(200,WIDTH-200), random(200,HEIGHT-200), DIAMETER);
+    }
+}
---
+++ src/browniantree/Particle.java
@@ -0,0 +1,40 @@
+package browniantree;
+
+import processing.core.PApplet;
+import processing.core.PVector;
+
+import java.util.List;
+
+public class Particle {
+    private final PApplet p;
+    private final PVector pos;
+    private final float diameter;
+
+    public Particle(PApplet parent, float x, float y, float diameter){
+        p = parent;
+        pos = new PVector(x, y);
+        this.diameter = diameter;
+    }
+
+    public void move(){
+        pos.add(PVector.random2D().mult(5));
+        pos.x = PApplet.constrain(pos.x, 0, BrownianTree.WIDTH);
+        pos.y = PApplet.constrain(pos.y, 0, BrownianTree.HEIGHT);
+    }
+
+    public void draw(){
+        p.circle(pos.x, pos.y, diameter);
+    }
+
+    public boolean checkIfCollides(List<Particle> frozenParticles){
+        for (int i = frozenParticles.size() - 1; i >= 0; i--) {
+            Particle frozenParticle = frozenParticles.get(i);
+
+            if (PVector.dist(pos, frozenParticle.pos) < diameter){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
